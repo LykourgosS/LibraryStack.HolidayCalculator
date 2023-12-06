48823adecd4d9bb2a2c94bdc3589dda4850f699e
LykourgosS <lpsarantidis@gmail.com>
2023-12-06 15:02:03 +0200

    Add IPublicHolidays interface

---
+++ LibraryStack.HolidayCalculator/IPublicHolidays.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryStack.HolidayCalculator
+{
+    internal interface IPublicHolidays
+    {
+        public List<DateTime> GetHolidays(int year);
+        public List<DateTime> GetHolidays(DateTime from, DateTime to);
+        public bool IsHoliday(DateTime date);
+        public bool IsWeekend(DateTime date);
+        public bool IsWorkDay(DateTime date);
+    }
+}
